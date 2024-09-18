8aadbcadf354d2b4e7c97dc52ba16becae111dd3
sandroluisj <sandroluisjr06@gmail.com>
2024-09-18 15:23:38 -0300

    add

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
-app.use (express.json());
+app.use(express.json());
 
 mongoose
-
-.connect("mongobd://localhost:27017/")
+.connect("mongodb://localhost:27017/livraria")
 .then(() => console.log("Conectado ao MongoDB"))
 .catch((erro) => console.error("Erro ao conectar ao MongoDB:", erro));
 const esquemaAluguel = new mongoose.Schema({
